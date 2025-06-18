bffc2cd13c3cb6ae070c6a50b8561be4db87c449
JanPanjan <89231282@student.upr.si>
2025-06-18 14:03:58 +0200

    for some reason ne dela setup od telescope..

--- lua/pogacha/telescope.lua
+++ lua/pogacha/telescope.lua
@@ -1,17 +1,34 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	enabled = true,
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', },
-		{ 'nvim-telescope/telescope-ui-select.nvim' },
-		{ 'nvim-tree/nvim-web-devicons',              enabled = vim.g.have_nerd_font },
-	},
-	config = function()
-		-- require('telescope').setup {
-		-- defaults = {
-		-- 	borderchars = { "█", " ", "▀", "█", "█", " ", " ", "▀" },
-		-- }
-		-- }
-	end
+  'nvim-telescope/telescope.nvim',
+  enabled = true,
+  dependencies = {
+    { 'nvim-lua/plenary.nvim' },
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    { 'nvim-telescope/telescope-ui-select.nvim' },
+    {
+      'nvim-tree/nvim-web-devicons',
+      enabled = vim.g.have_nerd_font
+    },
+  },
+  opts = {
+    extensions = {
+      fzf = {
+        fuzzy = true,                   -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true,    -- override the file sorter
+        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      }
+    },
+    defaults = {
+      border = false,
+      borderchars = { "█", " ", "▀", "█", "█", " ", " ", "▀" },
+    },
+  },
+  config = function()
+    -- require('telescope').setup({})
+  end
 }
